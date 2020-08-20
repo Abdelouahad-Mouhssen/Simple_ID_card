3dd629189a6ade4cafe3f51bc5d7453385613bdf
ABDELOUAHAD Mouhcen <mouhcenultra@hotmail.fr>
2020-08-20 16:49:23 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/mouhssenpic.jpg'),
+              ),
+              Text(
+                'Mouhssen ',
+                style: TextStyle(
+                    fontFamily: 'Alata-Regular',
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    letterSpacing: 0.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'UI/UX Designer',
+                style: TextStyle(
+                  fontFamily: 'Alata-Regular',
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  letterSpacing: 1.0,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+213 665 172 969',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'Mouhcenultra@hotmail.fr',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
